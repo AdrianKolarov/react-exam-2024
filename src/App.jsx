eedbba50f6f38ff26b4d9e1125a4b9f78421b0d9
Adrian Kolarov <adrian.kolarov@abv.bg>
2024-08-01 20:01:20 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,30 @@ import MyList from './components/MyList.jsx'
 import { Route, Routes } from "react-router-dom"
 import NotFound from './components/NotFound.jsx'
 import Characters from './components/Characters.jsx'
-function App() {
- 
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { onAuthStateChanged } from 'firebase/auth'
+import { setUser } from './store/authSlice.js'
+import { auth } from './firebase/config.js'
+import Details from './components/Details.jsx'
 
+
+
+
+
+function App() {
+
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    const unsub = onAuthStateChanged(auth, (user)=>{
+      if(user){
+        dispatch(setUser(user.uid))
+      } else {
+        dispatch(setUser(null))
+      }
+    })
+    return ()=>unsub()
+  },[dispatch])
   return (
     <>
       <NavBar/>
@@ -23,8 +44,9 @@ function App() {
         <Route path='/login' element={<Login />} />
         <Route path="/import" element={<ImportCharacter />}/>
         <Route path="/mylist" element={<MyList />}/>
-        <Route path="/*" element={<NotFound />}/>
         <Route path="/characters" element={<Characters/>}/>
+        <Route path="/*" element={<NotFound />}/>
+        <Route path="/details" element={<Details />} />
       </Routes>
       <Footer />
       
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,19 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom"
+import { Provider } from 'react-redux'
+import  store  from "./store/store"
+import  AuthProvider from "./components/AuthProvider.jsx"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
+
+<Provider store={store}>
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
+  </AuthProvider>
+</Provider>
   ,
 )
