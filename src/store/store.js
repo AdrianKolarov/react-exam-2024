69c7838c721ba86f770aa717c2c573367c53342d
Adrian Kolarov <adrian.kolarov@abv.bg>
2024-08-04 21:36:47 +0200

    Add files via upload
    App completed

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./usersSlice"; 
 import charactersReducer from "./characterSlice"
 import authReducer from "./authSlice";
+import userBlacklistReducer from "./userBlacklistSlice"
+import globalBlacklistReducer from "./globalBlacklistSlice"
 
 const store = configureStore({
     reducer: {
         users: usersReducer,
         characters: charactersReducer,
-        auth: authReducer
+        auth: authReducer,
+        userBlacklist: userBlacklistReducer,
+        globalBlacklist: globalBlacklistReducer
     },
 });
 
